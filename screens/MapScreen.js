7bae128a9daf78765dbe410dc8c34df604161226
suopoika <henrik.junttila@gmail.com>
2024-04-06 15:21:43 +0300

    kartan piirto, sijainti ja markker

---
+++ screens/MapScreen.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
+export default function MapScreen() {
+    const [location, setLocation] = useState({
+        latitude: 60.200692,
+        longitude: 24.934302,
+        latitudeDelta: 0.0322,
+        longitudeDelta: 0.0221,
+    });
+
+    useEffect(() => {
+        (async () => {
+            await getUserPosition();
+        })();
+    }, []);
+
+    const getUserPosition = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+
+        try {
+            if (status !== 'granted') {
+                console.log('Geolocation failed');
+                return;
+            }
+
+            const position = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High });
+            setLocation({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                latitudeDelta: 0.0322,
+                longitudeDelta: 0.0221,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <MapView
+            style={styles.map}
+            region={location}
+            mapType='standard'
+        >
+            <Marker
+                title='You are here'
+                coordinate={{ latitude: location.latitude, longitude: location.longitude }}
+                image={require('../assets/blue100.png')}
+            />
+        </MapView>
+    );
+}
+
+const styles = StyleSheet.create({
+    map: {
+        width: '100%',
+        height: '100%',
+    },
+});
