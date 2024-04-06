0cb07d40348ccaeab81d304d6e1facc6ceb25401
Olli Saukko <ollisaukko@Ollis-MacBook-Air.local>
2024-04-06 20:39:10 +0300

    navigationbar

---
+++ screens/index.js
@@ -0,0 +1,13 @@
+import HomeScreen from "./HomeScreen";
+import CalendarScreen from "./CalendarScreen";
+import AddWorkoutScreen from "./AddWorkoutScreen";
+import MapScreen from "./MapScreen";
+import ProfileScreen from "./ProfileScreen";
+
+export {
+  HomeScreen,
+  CalendarScreen,
+  AddWorkoutScreen,
+  MapScreen,
+  ProfileScreen,
+};
