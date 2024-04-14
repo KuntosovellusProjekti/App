1ff86f5566edc37b083a0c79b1660a33eefd87d2
Olli Saukko <ollisaukko@Ollis-MacBook-Air.local>
2024-04-14 18:09:30 +0300

    app.js

--- App.js
+++ App.js
@@ -3,16 +3,12 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Navigation from './components/Navigation/Navigation';
-import SignUpScreen from './screens/SignUpScreen';
-import LoginScreen from './screens/LoginScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <Navigation />
-      <StatusBar style="auto" /> */}
-< SignUpScreen />
-{/* <LoginScreen /> */}
+      <Navigation />
+      <StatusBar style="auto" />
     </View>
   );
 }
